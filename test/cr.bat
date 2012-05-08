9384e4ecdbf7ae324aa6cb5e3b0d7a9f313333c9
ed <edwardtsang@atti.com>
2012-05-08 00:43:15 -0700

    bbkjnlj

--- test/cr.bat
+++ test/cr.bat
@@ -1,6 +1,6 @@
 @echo on
 
 del WinNovaClient.exe
-.\7zr a archive.7z .\7zr.exe -m0=BCJ2 -m1=LZMA:d25:fb255 -m2=LZMA:d19 -m3=LZMA:d19 -mb0:1 -mb0s1:2 -mb0s2:3 -mx
+.\7zr a WinNovaClient.7z .\7zr.exe -m0=BCJ2 -m1=LZMA:d25:fb255 -m2=LZMA:d19 -m3=LZMA:d19 -mb0:1 -mb0s1:2 -mb0s2:3 -mx
 copy /b .\7zSD.sfx + config.txt + WinNovaClient.7z WinNovaClient.exe
 
