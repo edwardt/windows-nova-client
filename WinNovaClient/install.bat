8666a663aaf45822f357252c3b4575601851ee01
ed tsang <edwardt.tril@gmail.com>
2012-05-24 13:04:20 -0700

    updated script

--- WinNovaClient/install.bat
+++ WinNovaClient/install.bat
@@ -10,8 +10,17 @@ popd
 
 pushd
 echo "Installaing Nova Python Client for Windows" >> %LOGFILE%
-call pypm install python-novatools >> %LOGFILE%
+call pypm install python-novatools >> %LOGFILE% 
 echo "Done Installaing Nova Python Client for Windows" >> %LOGFILE%
 popd
+
+pushd
+echo "Try calling nova tool"
+call C:\Python27\Scripts\nova-script.py
+popd
+	
+echo "Please type anykey to continue ..."
+echo "Please check config.log for any error. Save config.log for reporting error"
+set /p Continue=
 
 endlocal
